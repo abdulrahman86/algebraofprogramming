4dbbed7ce74af87d3c2795e8cff0c39313cc64ee
asattar <abdulrahman86@gmail.com>
2015-09-06 21:50:25 -0400

    Definitions of following function in terms of fold -FoldIterative -takeN -dropN

--- src/main/scala/resources/worksheets/chapter1/chapter1.sc
+++ src/main/scala/resources/worksheets/chapter1/chapter1.sc
@@ -1,6 +1,13 @@
 package resources.worksheets.chapter1
 
+import sample.project.utils.MyUtils._
+
 object chapter1
 {
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  foldItrFromFoldRecur[Int, Int](6, _ + _) (List(1, 2, 3))
+                                                  //> res0: Int = 12
+                                                  
+  takeNUsingFold(List(1, 2, 3, 4, 5, 6))(7)       //> res1: List[Int] = List(1, 2, 3, 4, 5, 6)
+  
+  dropNUsingFold(List(1, 2, 3, 4))(2)             //> res2: List[Int] = List(3, 4)
 }
---
+++ src/main/scala/sample/project/utils/Utils.scala
@@ -0,0 +1,35 @@
+package sample.project.utils
+
+
+/**
+ * @author asattar
+ */
+package object MyUtils 
+{
+
+	def foldItrFromFoldRecur[A, B](c: B, h : (A, B) => B) (list: List[A]) = {
+
+		(fold[A, B => B] ((b) => b, (a, y) => ((b) => y(h(a, b))))(list))(c)
+	}
+
+	def fold[A, B](c: B, h : (A, B) => B) (list: List[A]) : B= {
+
+		list match {
+		case Nil => c
+
+		case head  :: rest => h(head, fold(c, h)(rest))
+		}
+	}
+
+	def takeNUsingFold[A] (l: List[A]) : (Int => List[A]) = {
+  
+    fold[A, Int => List[A]] (_ => List(), 
+        (a, b) => (n) => {if (n > 0) a :: b(n-1) else List()})(l)
+
+	}
+  
+  def dropNUsingFold[A] (l: List[A]) : (Int => List[A]) = {
+    fold[A, Int => List[A]] (_ => List(), 
+        (a, b) => (n) => {if (n > 0) b(n -1) else a :: b(n - 1)})(l)
+  }
+}
